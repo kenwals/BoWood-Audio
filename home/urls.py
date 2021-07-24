63a2d08207218eff8504494ca21786fe77889c16
kenwals <kenwals@gmail.com>
2021-07-24 17:25:21 +0100

    wire up pages and links

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('whatwedo/', views.whatwedo, name='whatwedo'),
     path('', views.index, name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,18 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def whatwedo(request):
+    """ A view to return the what we do page """
+
+    return render(request, 'home/whatwedo.html')
+
+def contact(request):
+    """ A view to return the contact page """
+
+    return render(request, 'home/contact.html')
+
+def gallery(request):
+    """ A view to return the gallery page """
+
+    return render(request, 'home/gallery.html')
