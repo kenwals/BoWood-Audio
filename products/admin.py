072fc53d828150e407f7dd3b392209da7f4ab09a
kenwals <kenwals@gmail.com>
2021-07-25 11:46:20 +0100

    customised list display on admin portal

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    ) # this controls what columns are on the list display in the admin portal
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
