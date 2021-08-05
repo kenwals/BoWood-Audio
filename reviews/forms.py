cfc1e2bbe16160848093442dfdd60fffc7ffa7d4
kenwals <kenwals@gmail.com>
2021-08-05 17:17:42 +0100

    set up model, form and template

---
+++ reviews/forms.py
@@ -0,0 +1,28 @@
+from products.models import Product
+from django import forms
+from django.forms import fields
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ('rating', 'review_title', 'review_text')
+        labels = {
+            'rating': 'Rating', 
+            'review_title': 'Title', 
+            'review_text': 'Review'
+        }
+
+        rating_choices = {
+            ('1', '1'),
+            ('2', '3'),
+            ('3', '3'),
+        }
+
+        widgets = {
+            'review_title': forms.TextInput(attrs={ 'placeholder':'Enter your review title here' }),
+            'review_text': forms.EmailInput(attrs={ 'placeholder':'Enter your review here' }),
+            'rating': forms.Select(choices=rating_choices)
+        }
+
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
+from products.models import Product
 from django.db import models
+from django.db.models.fields import DecimalField
 
-# Create your models here.
+from products.models import Product
+from profiles.models import UserProfile
+
+
+class Review(models.Model):
+
+    rating = models.DecimalField(max_digits=6, decimal_places=2)
+    userid = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    review_title = models.CharField(max_length=90)
+    review_text = models.CharField(max_length=3000)
+
