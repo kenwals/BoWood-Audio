19748813b14f9ada0d6764f217cb1c156b60f645
kenwals <kenwals@gmail.com>
2021-08-10 14:18:15 +0100

    feat signals added to the review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,8 @@
 from products.models import Product
 from django.db import models
 from django.db.models.fields import DecimalField
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 from products.models import Product
 from profiles.models import UserProfile
@@ -14,3 +16,20 @@ class Review(models.Model):
     review_title = models.CharField(max_length=90)
     review_text = models.TextField(max_length=3000)
 
+
+@receiver(post_save, sender=Review)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update products rating on review save/update 
+    """
+    if created: 
+        print("a new review has been created")
+    else:
+        print("a current review has been updated")
+
+@receiver(post_delete, sender=Review)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update products rating on review delete
+    """
+    print("a review has been deleted ")
