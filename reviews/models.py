a93033b405ab955b617e194d421c0a19023ef724
kenwals <kenwals@gmail.com>
2021-08-10 15:34:57 +0100

    feat review signal updates rating on products

--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,7 @@ from products.models import Product
 from django.db import models
 from django.db.models.fields import DecimalField
 from django.db.models.signals import post_save, post_delete
+from django.db.models import Avg
 from django.dispatch import receiver
 
 from products.models import Product
@@ -22,6 +23,11 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update products rating on review save/update 
     """
+    product = Product.objects.get(name=instance.product)
+    reviews = Review.objects.filter(product=product)
+    avg_rating = reviews.aggregate(Avg('rating'))['rating__avg']
+    product.rating = int(avg_rating)
+    product.save()
     if created: 
         print("a new review has been created")
     else:
@@ -32,4 +38,12 @@ def update_on_delete(sender, instance, **kwargs):
     """
     Update products rating on review delete
     """
+    product = Product.objects.get(name=instance.product)
+    reviews = Review.objects.filter(product=product)
+    avg_rating = reviews.aggregate(Avg('rating'))['rating__avg']
+    if avg_rating:
+        product.rating = int(avg_rating)
+    else:
+        product.rating = 0
+    product.save()
     print("a review has been deleted ")
