2e99dabf1412a817d5882e68944edc9d6cb7c600
kenwals <kenwals@gmail.com>
2021-08-17 18:22:38 +0100

    refactor change limit on pagination

--- products/views.py
+++ products/views.py
@@ -52,7 +52,7 @@ def all_products(request):
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
-    paginator = Paginator(products, 4)  # Show 4 products per page.
+    paginator = Paginator(products, 12)  # Show 12 products per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -72,7 +72,7 @@ def product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
     reviews = Review.objects.filter(product=product_id)
-    paginator = Paginator(reviews, 2)  # shows 2 reviews per page.
+    paginator = Paginator(reviews, 5)  # shows 5 reviews per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     form = ReviewForm()
